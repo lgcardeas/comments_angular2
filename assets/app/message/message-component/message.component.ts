edabaa78f29132627fd391cf814ecc6e00490ee1
Luis Ernesto <lgcardenas910526@gmail.com>
2017-02-22 20:50:52 -0500

    Preparing the branch to merge with master branch

--- assets/app/message/message-component/message.component.ts
+++ assets/app/message/message-component/message.component.ts
@@ -40,4 +40,8 @@ export class MessageComponent {
                 result => console.log(result)
             );
   }
+
+  belongsToUser(){
+      return localStorage.getItem('userId') == this.message.userId;
+  }
 }
